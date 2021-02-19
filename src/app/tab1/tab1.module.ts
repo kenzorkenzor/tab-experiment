32e57a6551c87d4c7a1fecd0d55aa47323bc1cfe
kenzor <email@example.org>
2021-02-19 11:55:39 +1100

    fix import

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { TabItemPage } from './tab-item.page';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
@@ -13,6 +14,6 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     FormsModule,
     Tab1PageRoutingModule
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page, TabItemPage]
 })
 export class Tab1PageModule {}
--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
+import { TabItemPage } from './tab-item.page';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
@@ -15,6 +16,6 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     RouterModule.forChild([{ path: '', component: Tab3Page }]),
     Tab3PageRoutingModule,
   ],
-  declarations: [Tab3Page]
+  declarations: [Tab3Page, TabItemPage]
 })
 export class Tab3PageModule {}
